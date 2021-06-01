33341d8f7bfe51f66bd05f90bec02c300544623a
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-01 12:28:26 +0200

    Ejercicio completado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
+// Generación de número aleatorio
 const numeroAleatorio = Math.ceil(Math.random() * 10);
 
-console.log(numeroAleatorio);
+// Llamada a botón y evento click
+const botonAdivinar = document.querySelector(".adivinar");
+const mensajeElemento = document.querySelector(".mensaje");
+
+botonAdivinar.addEventListener("click", () => {
+  const inputNumero = document.querySelector(".numero");
+  const numeroUsuario = inputNumero.valueAsNumber;
+
+  if (numeroUsuario > numeroAleatorio) {
+    mensajeElemento.textContent = `El número es menor que ${numeroUsuario}`;
+    mensajeElemento.classList.add("error");
+  } else if (numeroUsuario < numeroAleatorio) {
+    mensajeElemento.textContent = `El número es mayor que ${numeroUsuario}`;
+    mensajeElemento.classList.add("error");
+  } else {
+    mensajeElemento.textContent = "Has acertado";
+    mensajeElemento.classList.add("acierto");
+    botonAdivinar.disabled = true;
+    inputNumero.disabled = true;
+  }
+});
